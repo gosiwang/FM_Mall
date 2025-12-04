ef6a3984b98f6e604f81f854332c91163f7cf3db
moonwonkim-a <happynei200@gmail.com>
2025-12-04 18:13:17 +0900

    feat(cart): Cart 엔티티에 장바구니 품목 관리 로직 보강

--- fmmall/src/main/java/com/sesac/fmmall/Entity/Cart.java
+++ fmmall/src/main/java/com/sesac/fmmall/Entity/Cart.java
@@ -18,9 +18,11 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cartId;
+
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CartItem> cartItems = new ArrayList<>();
 
